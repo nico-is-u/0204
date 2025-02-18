2a0dc8e84797cefc4b9f17822b31ea95e5680303
Archiewuwu <archiewuwu@gmail.com>
2025-02-18 22:40:12 +0800

    '分享好友'

--- pages/invite_friend.vue
+++ pages/invite_friend.vue
@@ -3,50 +3,46 @@
     <view class="page_invite_friend">
         <nNavbar title="" :back="true" :isFixed="true"></nNavbar>
 
-        <view class="invite_friend">
-            <view class="white_box flex flex-column flex-y-center">
-                
-                <view class="title">
-                    邀请好友
+        <view class="white_box flex flex-column flex-y-center">
+            
+            <view class="title">
+                分享好友
+            </view>
+            <view class="tips">
+                复制下方邀请码或推荐码分享好友
+            </view>
+            <view class="yellow_box">
+                <view class="code">
+                    
+                    <image v-if="qrCodeBase64" :src="qrCodeBase64" mode="widthFix"></image>
                 </view>
-                <view class="tips">
-                    复制下方邀请码或推荐码分享好友
+                <view class="line flex flex-center">
+                    <view class="left_text">
+                        邀请码：
+                    </view>
+                    <view class="center">
+                        {{ invite_code }}
+                    </view>
+                    <view class="right">
+
+                        <button @click="copyData(invite_code)">复制</button>
+                    </view>
                 </view>
-                <view class="yellow_box">
-                    <view class="code">
-                        
-                        <image v-if="qrCodeBase64" :src="qrCodeBase64" mode="widthFix"></image>
+                <view class="line flex flex-center">
+                    <view class="left_text">
+                        邀请链接：
                     </view>
-                    <view class="line flex flex-center">
-                        <view class="left_text">
-                            邀请码：
-                        </view>
-                        <view class="center">
-                            {{ invite_code }}
-                        </view>
-                        <view class="right">
+                    <view class="center">
+                        {{ invite_url }}
+                    </view>
+                    <view class="right">
+                        <button @click="copyData(invite_url)">复制</button>
 
-                            <button>复制</button>
-                        </view>
                     </view>
-                    <view class="line flex flex-center">
-                        <view class="left_text">
-                            邀请链接：
-                        </view>
-                        <view class="center">
-                            {{ invite_url }}
-                        </view>
-                        <view class="right">
-                            <button>复制</button>
-
-                        </view>
-                        
-                    </view>
+                    
                 </view>
             </view>
-
         </view>
-            
 
 
 
@@ -80,14 +76,20 @@
         },
 		methods: {
 			async createQRCode(text) {
-            try {
-                // 生成二维码
-                this.qrCodeBase64 = await QRCode.toDataURL(text);
-                console.log(this.qrCodeBase64)
-            } catch (error) {
-                console.error('生成二维码失败:', error);
-            }
-            }
+                try {
+                    // 生成二维码
+                    this.qrCodeBase64 = await QRCode.toDataURL(text);
+                    console.log(this.qrCodeBase64)
+                } catch (error) {
+                    console.error('生成二维码失败:', error);
+                }
+            },
+            copyData(val) {
+                uni.setClipboardData({
+                    data: val
+                })
+                this.toa('复制成功')
+            },
 		}
 	}
 </script>
@@ -110,72 +112,70 @@ page{
 
 .page_invite_friend{
 
-    .invite_friend{
-        
-        padding-top: 660rpx;
-        
-        .white_box{
-            background-image: url("/static/invite_friend_whiteBox.png");
-            background-size: 100% 100%;
-            
-            padding: 0 60rpx 30rpx 60rpx;
-            .title{
-                margin-top: 50rpx;
-                font-size: 36rpx;
-                color: #7f4c2d;
-            }
-            .tips{
-                margin-top: 30rpx;
-                font-size: 30rpx;
-                color: #7f4c2d;
-            }
-            .yellow_box{
-                width: 100%;
-                margin: 30rpx 0;
-                background-image: linear-gradient(to bottom, #fbf5ec, #fbf0d8); 
-
-                // 二维码
-                .code{
-                    padding: 50rpx 100rpx;
-                    width: 100%;
-                    image{
-                        width: 100%;
-                        
-                    }
-                }
-                    
-                .line{
-                    width: 100%;
-                    padding: 10rpx 60rpx;
-                    .left_text{
-                        font-size: 30rpx;
-                        width: 30%;                
-                    }
-                    .center{
-                        margin-right: 30rpx;
-                        padding: 4rpx 0 4rpx 30rpx;
-                        background-color: #eddebf;
-                        border-radius: 80rpx;
-                        font-size: 30rpx;
-                        width: 40%;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                    }
-                    .right{
-                        width: 30%;
-                        button{
-                            font-size: 30rpx;
-                            color: white;
-                            padding: 10rpx 30rpx;
-                            border-radius: 80rpx;
-                            background-image: linear-gradient(to right, #e93b0c, #a11509); 
-                            line-height: 1;
-                        }
-                    }
-                }
-            }
+    padding-top: 660rpx;
     
+    .white_box{
+        background-image: url("/static/invite_friend_whiteBox.png");
+        background-size: 100% 100%;
+        
+        padding: 0 60rpx 30rpx 60rpx;
+        .title{
+            margin-top: 40rpx;
+            font-size: 36rpx;
+            color: #7f4c2d;
         }
+        .tips{
+            margin-top: 30rpx;
+            font-size: 30rpx;
+            color: #7f4c2d;
+        }
+        .yellow_box{
+            width: 100%;
+            margin: 30rpx 0;
+            padding-bottom: 30rpx;
+            background-image: linear-gradient(to bottom, #fbf5ec, #fbf0d8); 
+
+            // 二维码
+            .code{
+                padding: 50rpx 100rpx;
+                width: 100%;
+                image{
+                    width: 100%;
+                    
+                }
+            }
+                
+            .line{
+                width: 100%;
+                padding: 16rpx 30rpx;
+                .left_text{
+                    font-size: 30rpx;
+                    width: 30%;                
+                }
+                .center{
+                    margin-right: 30rpx;
+                    padding: 4rpx 0 4rpx 30rpx;
+                    background-color: #eddebf;
+                    border-radius: 80rpx;
+                    font-size: 30rpx;
+                    width: 40%;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                }
+                .right{
+                    width: 30%;
+                    button{
+                        font-size: 30rpx;
+                        color: white;
+                        padding: 10rpx 30rpx;
+                        border-radius: 80rpx;
+                        background-image: linear-gradient(to right, #e93b0c, #a11509); 
+                        line-height: 1;
+                    }
+                }
+            }
+        }
+
     }
 
 }
