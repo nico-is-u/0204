94e8481b3343a630dffee0a59ed094f1368f5d2e
Archiewuwu <archiewuwu@gmail.com>
2025-02-18 21:16:09 +0800

    '邀请好友'

--- pages/invite_friend.vue
+++ pages/invite_friend.vue
@@ -3,13 +3,39 @@
     <view class="page_invite_friend">
         <nNavbar title="" :back="true" :isFixed="true"></nNavbar>
 
-
         <view class="invite_friend">
-            <view class="white_box">
-                <image src="/static/invite_friend_whiteBox.png"></image>
+            <view class="white_box flex flex-column flex-y-center">
+                
+                <view class="title">
+                    邀请好友
+                </view>
+                <view class="tips">
+                    复制下方邀请码或推荐码分享好友
+                </view>
+                <view class="yellow_box">
+                    <view class="code">
+                        <image src="/static/invite_friend_code.png"></image>
+                    </view>
+                    <view class="line flex flex-center">
+                        <view class="left_text">
+                            邀请码：
+                        </view>
+                        <input type="text">
+                        <button>复制</button>
+                    </view>
+                    <view class="line flex flex-center">
+                        <view class="left_text">
+                            邀请链接：
+                        </view>
+                        <input type="text">
+                        <button>复制</button>
+                        
+                    </view>
+                </view>
             </view>
 
         </view>
+            
 
 
 
@@ -48,25 +74,71 @@ page{
     background-color: #ea5c60;
     background-size: 100% 100%;
 	background-repeat: no-repeat;
+    
 }
 
 .page_invite_friend{
-    height: 100%;
-    width: 100%;
+
     .invite_friend{
-        height: 100%;
-        width: 100%;
-        padding-top: 860rpx;
+        
+        padding-top: 820rpx;
+
         .white_box{
-            height: 100%;
-            width: 100%;
-            image{
-                height: 100%;
-                // width: 100%;
+            background-image: url("/static/invite_friend_whiteBox.png");
+            background-size: 100% 100%;
+
+            padding: 0 60rpx;
+            .title{
+                margin-top: 50rpx;
+                font-size: 36rpx;
+                color: #7f4c2d;
             }
+            .tips{
+                margin-top: 30rpx;
+                font-size: 30rpx;
+                color: #7f4c2d;
+            }
+            .yellow_box{
+                width: 100%;
+                margin: 30rpx 0;
+                background-image: linear-gradient(to bottom, #fbf5ec, #fbf0d8); 
 
+                // 二维码
+                .code{
+                    padding: 50rpx 100rpx;
+                    width: 100%;
+                    image{
+                        width: 100%;
+                        
+                    }
+                }
+                    
+                .line{
+                    width: 100%;
+                    padding: 0 60rpx;
+                    .left_text{
+                        font-size: 30rpx;
+                        width: 30%;                
+                    }
+                    input{
+                        margin-right: 30rpx;
+                        background-color: #eddebf;
+                        border-radius: 80rpx;
+                        font-size: 30rpx;
+                        width: 50%;
+                    }
+                    button{
+                        font-size: 30rpx;
+                        color: white;
+                        padding: 0;
+                        border-radius: 80rpx;
+                        background-image: linear-gradient(to right, #e93b0c, #a11509); 
+                        width: 20%;
+                    }
+                }
+            }
+    
         }
-
     }
 
 }
