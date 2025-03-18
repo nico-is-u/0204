90d21c61823516687407ee7b8c78f8f9e290a566
nico_is_u <308878328@qq.com>
2025-03-18 09:26:50 +0700

    ...

--- pages/index.vue
+++ pages/index.vue
@@ -113,10 +113,11 @@
 		},
 		methods: {
 			getSystem_config() {
+				this.setting_config = uni.getStorageSync('setting_config')
 				this.to.www(this.api.system_info)
 					.then(res => {
-						this.setting_config = res.data.setting_conf;
-						uni.setStorageSync('setting_config',this.setting_config)
+						this.setting_config = res.data.setting_conf
+						uni.setStorageSync('setting_config',res.data.setting_conf)
 					})
 			},
 			getNewsList() {
